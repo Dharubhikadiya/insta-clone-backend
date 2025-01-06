ffbc3ba155d3107275cb3193657e9062d5c4ca48
Dharubhikadiya <bhikadiyadharmi55@gmail.com>
2025-01-06 23:51:24 +0530

    first commit

--- app.js
+++ app.js
@@ -11,7 +11,13 @@ connectToDb();
 
 // Middleware
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL || "http://localhost:3000",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
 
 // Models
 require("./models/model");
